550856898f155ea624ffd6fe569edf452d666663
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-08-23 05:53:17 -0300

    feat(UserTest): add shouldThrowExceptionWhenInstantiateUserWithNullEmail function

--- src/test/java/coursesystem/domain/entities/user/UserTest.java
+++ src/test/java/coursesystem/domain/entities/user/UserTest.java
@@ -1,6 +1,7 @@
 package coursesystem.domain.entities.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +9,20 @@ public class UserTest {
   @Test
   void shouldInstantiateUserWithValidData() {
     String email = "daniel.s.t.shimabukuro@gmail.com";
+    String name = "Daniel";
+    String password = "Senha";
     
 
-    User user = new User("daniel.s.t.shimabukuro@gmail.com", "Daniel", "Senha");
+    User user = new User(email, name, password);
 
-    assertEquals("daniel.s.t.shimabukuro@gmail.com", user.email());
-    assertEquals("Daniel", user.username());
-    assertEquals("Senha", user.password());
-  }  
+    assertEquals(email, user.email());
+    assertEquals(name, user.username());
+    assertEquals(password, user.password());
+  }
+
+  @Test
+  void shouldThrowExceptionWhenInstantiateUserWithNullEmail() {
+    IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> new User(null, "Daniel", "Senha"));
+    assertEquals("Invalid Email.", ex.getMessage());
+  }
 }
