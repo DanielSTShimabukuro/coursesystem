4d409fa04e6099804e71568666dd2e36ba26fd24
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-08-31 18:27:50 -0300

    feat(CourseRepository): add interface

---
+++ src/main/java/coursesystem/infrastructure/persistence/course/CourseRepository.java
@@ -0,0 +1,9 @@
+package coursesystem.infrastructure.persistence.course;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
+  
+}
