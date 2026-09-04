e8dda9e0c1b9192cfa33d00fd8ff74d7c19439a5
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-04 18:52:47 -0300

    feat(UserDTOMapper): add toOutput function

--- src/main/java/coursesystem/application/dto/mapper/UserDTOMapper.java
+++ src/main/java/coursesystem/application/dto/mapper/UserDTOMapper.java
@@ -1,10 +1,20 @@
 package coursesystem.application.dto.mapper;
 
 import coursesystem.application.dto.input.user.CreateUserInputDTO;
+import coursesystem.application.dto.output.user.UserOutputDTO;
 import coursesystem.domain.model.user.User;
 
 public class UserDTOMapper {
   public User toDomain(CreateUserInputDTO input) {
     return new User(input.email(), input.username(), input.password());
+  }
+
+  public UserOutputDTO toOutput(User user) {
+    return new UserOutputDTO(user.id(),
+                              user.email(),
+                              user.username(),
+                              user.createdAt(),
+                              user.updatedAt(),
+                              user.userCourseIds());
   }
 }
