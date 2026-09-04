811f0a3a49eded20d0442a1dd22ea51ea7afba88
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-04 20:34:28 -0300

    feat(UserPresentationMapper): add toResponse function

--- src/main/java/coursesystem/presentation/mapper/UserPresentationMapper.java
+++ src/main/java/coursesystem/presentation/mapper/UserPresentationMapper.java
@@ -1,10 +1,21 @@
 package coursesystem.presentation.mapper;
 
 import coursesystem.application.input.user.CreateUserInputDTO;
+import coursesystem.application.output.user.UserOutputDTO;
 import coursesystem.presentation.request.user.CreateUserRequestDTO;
+import coursesystem.presentation.response.user.UserResponseDTO;
 
 public class UserPresentationMapper {
   public CreateUserInputDTO toInput(CreateUserRequestDTO request) {
     return new CreateUserInputDTO(request.email(), request.username(), request.password());
+  }
+
+  public UserResponseDTO toResponse(UserOutputDTO output) {
+    return new UserResponseDTO(output.id(),
+                              output.email(),
+                              output.username(),
+                              output.createdAt(),
+                              output.updatedAt(),
+                              output.userCourseIds());
   }
 }
