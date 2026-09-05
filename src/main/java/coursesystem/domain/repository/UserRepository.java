4913c0b29c0ba1b7f513eebec72c0ce12ee33268
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-05 17:15:11 -0300

    feat(UserRepository): add existsByEmailAndIdNot function

--- src/main/java/coursesystem/domain/repository/UserRepository.java
+++ src/main/java/coursesystem/domain/repository/UserRepository.java
@@ -9,6 +9,7 @@ public interface UserRepository {
   User save(User user);
 
   boolean existsByEmail(String email);
+  boolean existsByEmailAndIdNot(String email, UUID id);
   boolean existsByUsername(String username);
 
   Optional<User> findById(UUID id);
