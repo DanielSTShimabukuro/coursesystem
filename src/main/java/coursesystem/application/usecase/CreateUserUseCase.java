fd8b75750dbaf9e53609a775af2385f8ff312647
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-05 17:09:01 -0300

    feat(CreateUserUseCase): add validateUser function

--- src/main/java/coursesystem/application/usecase/CreateUserUseCase.java
+++ src/main/java/coursesystem/application/usecase/CreateUserUseCase.java
@@ -1,5 +1,6 @@
 package coursesystem.application.usecase;
 
+import coursesystem.application.exceptions.BusinessException;
 import coursesystem.application.input.user.CreateUserInputDTO;
 import coursesystem.application.mapper.UserApplicationMapper;
 import coursesystem.application.output.user.UserOutputDTO;
@@ -20,8 +21,16 @@ public class CreateUserUseCase {
   public UserOutputDTO execute(CreateUserInputDTO input) {
     User user = this.mapper.toDomain(input);
 
+    this.validateUser(user);
+
     user = this.repository.save(user);
 
     return this.mapper.toOutput(user);
+  }
+
+  private void validateUser(User user) {
+    if (this.repository.existsByEmail(user.email())) throw new BusinessException("Email Already Exists.");
+
+    if (this.repository.existsByUsername(user.username())) throw new BusinessException("Username Already Exists.");
   }
 }
