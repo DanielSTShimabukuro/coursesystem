71e06d96c779ac509c8cb482842352a540b7fbb2
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-05 17:57:40 -0300

    feat(UserPresentationMapper): add toInput function

--- src/main/java/coursesystem/presentation/mapper/UserPresentationMapper.java
+++ src/main/java/coursesystem/presentation/mapper/UserPresentationMapper.java
@@ -1,13 +1,19 @@
 package coursesystem.presentation.mapper;
 
 import coursesystem.application.input.user.CreateUserInputDTO;
+import coursesystem.application.input.user.UpdateUserInputDTO;
 import coursesystem.application.output.user.UserOutputDTO;
 import coursesystem.presentation.request.user.CreateUserRequestDTO;
+import coursesystem.presentation.request.user.UpdateUserRequestDTO;
 import coursesystem.presentation.response.user.UserResponseDTO;
 
 public class UserPresentationMapper {
   public CreateUserInputDTO toInput(CreateUserRequestDTO request) {
     return new CreateUserInputDTO(request.email(), request.username(), request.password());
+  }
+
+  public UpdateUserInputDTO toInput(UpdateUserRequestDTO request) {
+    return new UpdateUserInputDTO(request.email(), request.username(), request.password());
   }
 
   public UserResponseDTO toResponse(UserOutputDTO output) {
