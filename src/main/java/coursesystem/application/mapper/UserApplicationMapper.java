4df4e8789719a7232b55027a4432e3ba3b29078b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-05 17:44:01 -0300

    feat(UserApplicationMapper): add update function

--- src/main/java/coursesystem/application/mapper/UserApplicationMapper.java
+++ src/main/java/coursesystem/application/mapper/UserApplicationMapper.java
@@ -1,12 +1,23 @@
 package coursesystem.application.mapper;
 
 import coursesystem.application.input.user.CreateUserInputDTO;
+import coursesystem.application.input.user.UpdateUserInputDTO;
 import coursesystem.application.output.user.UserOutputDTO;
 import coursesystem.domain.model.user.User;
 
 public class UserApplicationMapper {
   public User toDomain(CreateUserInputDTO input) {
     return new User(input.email(), input.username(), input.password());
+  }
+
+  public User update(User user, UpdateUserInputDTO input) {
+    return new User(user.id(),
+                    input.email(), 
+                    input.username(), 
+                    input.password(), 
+                    user.createdAt(), 
+                    user.updatedAt(), 
+                    user.userCourseIds());
   }
 
   public UserOutputDTO toOutput(User user) {
