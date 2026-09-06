bc205fb8deb88af2558104ab0bbaeb3183aca22d
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-05 23:35:11 -0300

    feat(DeleteUserUseCase): add execute function

---
+++ src/main/java/coursesystem/application/usecase/DeleteUserUseCase.java
@@ -0,0 +1,21 @@
+package coursesystem.application.usecase;
+
+import java.util.UUID;
+
+import coursesystem.application.exceptions.NotFoundException;
+import coursesystem.domain.model.user.User;
+import coursesystem.domain.repository.UserRepository;
+
+public class DeleteUserUseCase {
+  private final UserRepository repository;
+
+  public DeleteUserUseCase(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  public void execute(UUID id) {
+    User user = this.repository.findById(id).orElseThrow(() -> new NotFoundException("User Not Found."));
+
+    this.repository.delete(user);
+  }
+}
