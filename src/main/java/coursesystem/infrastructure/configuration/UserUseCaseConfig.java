655bc890e6eeaf4afdf2fd97ff637fe25864b91c
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-06 00:11:12 -0300

    feat(UserUseCaseConfig): add deleteUserUseCase function

--- src/main/java/coursesystem/infrastructure/configuration/UserUseCaseConfig.java
+++ src/main/java/coursesystem/infrastructure/configuration/UserUseCaseConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import coursesystem.application.mapper.UserApplicationMapper;
 import coursesystem.application.usecase.CreateUserUseCase;
+import coursesystem.application.usecase.DeleteUserUseCase;
 import coursesystem.application.usecase.FindUserByIdUseCase;
 import coursesystem.application.usecase.UpdateUserUseCase;
 import coursesystem.domain.repository.UserRepository;
@@ -24,5 +25,10 @@ public class UserUseCaseConfig {
   @Bean 
   UpdateUserUseCase updateUserUseCase(UserApplicationMapper mapper, UserRepository repository) {
     return new UpdateUserUseCase(mapper, repository);
+  }
+
+  @Bean
+  DeleteUserUseCase deleteUserUseCase(UserRepository repository) {
+    return new DeleteUserUseCase(repository);
   }
 }
