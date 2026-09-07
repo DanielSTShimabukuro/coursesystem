9ff00fbd099e2e48e6820de9d92c8951be4f5ec6
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-07 09:36:46 -0300

    feat(CourseApplicationMapper): add toOutput function

--- src/main/java/coursesystem/application/mapper/CourseApplicationMapper.java
+++ src/main/java/coursesystem/application/mapper/CourseApplicationMapper.java
@@ -1,10 +1,20 @@
 package coursesystem.application.mapper;
 
 import coursesystem.application.input.course.CreateCourseInputDTO;
+import coursesystem.application.output.course.CourseOutputDTO;
 import coursesystem.domain.model.course.Course;
 
 public class CourseApplicationMapper {
   public Course toDomain(CreateCourseInputDTO input) {
     return new Course(input.name(), input.description(), input.price());
+  }
+
+  public CourseOutputDTO toOutput(Course course) {
+    return new CourseOutputDTO(course.id(), 
+                                course.name(),
+                                course.description(), 
+                                course.price(), 
+                                course.createdAt(),
+                                course.userCourseIds());   
   }
 }
