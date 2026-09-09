b3db3503e34ca47ef76e79e70bfd3353d17e5f8e
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-08 22:13:14 -0300

    feat(CourseUseCaseConfig): add createCourseUseCase function

--- src/main/java/coursesystem/infrastructure/configuration/CourseUseCaseConfig.java
+++ src/main/java/coursesystem/infrastructure/configuration/CourseUseCaseConfig.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import coursesystem.application.mapper.CourseApplicationMapper;
+import coursesystem.application.usecase.course.CreateCourseUseCase;
+import coursesystem.domain.repository.CourseRepository;
+import coursesystem.domain.repository.UserRepository;
 
 @Configuration 
 public class CourseUseCaseConfig {
   @Bean 
   CourseApplicationMapper courseApplicationMapper() {
     return new CourseApplicationMapper();
+  }
+
+  @Bean 
+  CreateCourseUseCase createCourseUseCase(CourseApplicationMapper mapper, UserRepository userRepository, CourseRepository repository) {
+    return new CreateCourseUseCase(mapper, userRepository, repository);
   }
 }
