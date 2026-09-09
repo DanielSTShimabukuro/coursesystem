47505e1b0ff3b5aafee8cb494d66ba8d6176767a
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-08 21:45:46 -0300

    feat(CourseResponseDTO): add toResponse function

--- src/main/java/coursesystem/presentation/mapper/CoursePresentationMapper.java
+++ src/main/java/coursesystem/presentation/mapper/CoursePresentationMapper.java
@@ -5,11 +5,21 @@ import java.util.UUID;
 import org.springframework.stereotype.Component;
 
 import coursesystem.application.input.course.CreateCourseInputDTO;
+import coursesystem.application.output.course.CourseOutputDTO;
 import coursesystem.presentation.request.course.CreateCourseRequestDTO;
+import coursesystem.presentation.response.course.CourseResponseDTO;
 
 @Component 
 public class CoursePresentationMapper {
   public CreateCourseInputDTO toInput(CreateCourseRequestDTO request, UUID userId) {
     return new CreateCourseInputDTO(request.name(), request.description(), request.price(), userId);
+  }
+
+  public CourseResponseDTO toResponse(CourseOutputDTO output) {
+    return new CourseResponseDTO(output.id(), 
+                                  output.name(), 
+                                  output.description(), 
+                                  output.price(), 
+                                  output.userCourseIds());
   }
 }
