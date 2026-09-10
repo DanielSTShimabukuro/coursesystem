7fc797d42e2ddaf4afb7759d3c407a616fa0c0d9
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-09 21:43:16 -0300

    feat(UserController): add debitBalanceUser function

--- src/main/java/coursesystem/presentation/controller/UserController.java
+++ src/main/java/coursesystem/presentation/controller/UserController.java
@@ -16,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import coursesystem.application.input.user.CreateUserInputDTO;
 import coursesystem.application.input.user.CreditBalanceUserInputDTO;
+import coursesystem.application.input.user.DebitBalanceUserInputDTO;
 import coursesystem.application.input.user.UpdateUserInputDTO;
 import coursesystem.application.output.user.UserOutputDTO;
 import coursesystem.application.usecase.user.CreateUserUseCase;
 import coursesystem.application.usecase.user.CreditBalanceUserUseCase;
+import coursesystem.application.usecase.user.DebitBalanceUserUseCase;
 import coursesystem.application.usecase.user.DeleteUserUseCase;
 import coursesystem.application.usecase.user.FindUserByIdUseCase;
 import coursesystem.application.usecase.user.UpdateUserUseCase;
 import coursesystem.presentation.mapper.UserPresentationMapper;
 import coursesystem.presentation.request.user.CreateUserRequestDTO;
 import coursesystem.presentation.request.user.CreditBalanceUserRequestDTO;
+import coursesystem.presentation.request.user.DebitBalanceUserRequestDTO;
 import coursesystem.presentation.request.user.UpdateUserRequestDTO;
 import coursesystem.presentation.response.user.UserResponseDTO;
 import jakarta.validation.Valid;
@@ -41,6 +44,7 @@ public class UserController {
   private final FindUserByIdUseCase findUserByIdUseCase;
   private final UpdateUserUseCase updateUserUseCase;
   private final CreditBalanceUserUseCase creditBalanceUserUseCase;
+  private final DebitBalanceUserUseCase debitBalanceUserUseCase;
   private final DeleteUserUseCase deleteUserUseCase;
 
   @ResponseStatus(HttpStatus.CREATED) 
@@ -77,6 +81,15 @@ public class UserController {
 
     return this.mapper.toResponse(output);
   } 
+
+  @ResponseStatus (HttpStatus.OK)
+  @PatchMapping("/debit")
+  public UserResponseDTO debitBalanceUser(@RequestBody @Valid DebitBalanceUserRequestDTO request) {
+    DebitBalanceUserInputDTO input = this.mapper.toInput(request);
+    UserOutputDTO output = this.debitBalanceUserUseCase.execute(input);
+
+    return this.mapper.toResponse(output);
+  }
 
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @DeleteMapping
