ceee7830fc49446f20d065d2cf22fbb42e426886
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-09 21:37:08 -0300

    feat(DebitBalanceUserUseCase): add execute function

--- src/main/java/coursesystem/application/usecase/user/DebitBalanceUserUseCase.java
+++ src/main/java/coursesystem/application/usecase/user/DebitBalanceUserUseCase.java
@@ -1,28 +1,28 @@
 package coursesystem.application.usecase.user;
 
 import coursesystem.application.exceptions.NotFoundException;
-import coursesystem.application.input.user.CreditBalanceUserInputDTO;
+import coursesystem.application.input.user.DebitBalanceUserInputDTO;
 import coursesystem.application.mapper.UserApplicationMapper;
 import coursesystem.application.output.user.UserOutputDTO;
 import coursesystem.domain.model.user.User;
 import coursesystem.domain.repository.UserRepository;
 
-public class CreditBalanceUserUseCase {
+public class DebitBalanceUserUseCase {
   private final UserApplicationMapper mapper;
   private final UserRepository repository;
 
-  public CreditBalanceUserUseCase(UserApplicationMapper mapper,
-                                    UserRepository repository) {
+  public DebitBalanceUserUseCase(UserApplicationMapper mapper,
+                                  UserRepository repository) {
     this.mapper = mapper;
-    this.repository = repository;
+    this.repository = repository;                                  
   }
 
-  public UserOutputDTO execute(CreditBalanceUserInputDTO input) {
+  public UserOutputDTO execute(DebitBalanceUserInputDTO input) {
     User user = this.repository.findById(input.id()).orElseThrow(() -> new NotFoundException("User Not Found."));
     
     user = this.mapper.update(user, input);
     this.repository.save(user);
-    
+
     return this.mapper.toOutput(user);
   }
 }
