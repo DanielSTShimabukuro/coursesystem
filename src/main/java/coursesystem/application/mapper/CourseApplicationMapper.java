1cd5a792579a473e11d115eb478a6c36ae4c52a1
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-10 20:35:48 -0300

    feat(CourseApplicationMapper): add update function

--- src/main/java/coursesystem/application/mapper/CourseApplicationMapper.java
+++ src/main/java/coursesystem/application/mapper/CourseApplicationMapper.java
@@ -1,12 +1,22 @@
 package coursesystem.application.mapper;
 
 import coursesystem.application.input.course.CreateCourseInputDTO;
+import coursesystem.application.input.course.UpdateCourseInputDTO;
 import coursesystem.application.output.course.CourseOutputDTO;
 import coursesystem.domain.model.course.Course;
 
 public class CourseApplicationMapper {
   public Course toDomain(CreateCourseInputDTO input) {
     return new Course(input.name(), input.description(), input.price());
+  }
+
+  public Course update(Course course, UpdateCourseInputDTO input) {
+    return new Course(course.id(), 
+                      input.name(), 
+                      input.description(), 
+                      input.price(), 
+                      course.createdAt(), 
+                      course.userCourseIds());
   }
 
   public CourseOutputDTO toOutput(Course course) {
