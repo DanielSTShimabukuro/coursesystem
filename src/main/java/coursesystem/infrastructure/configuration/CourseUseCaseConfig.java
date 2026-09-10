42938dd1240f6013c04ed437cab6dd96b40f5b44
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-09 22:32:11 -0300

    feat(CourseUseCaseConfig): add findByIdCourseUseCase function

--- src/main/java/coursesystem/infrastructure/configuration/CourseUseCaseConfig.java
+++ src/main/java/coursesystem/infrastructure/configuration/CourseUseCaseConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import coursesystem.application.mapper.CourseApplicationMapper;
 import coursesystem.application.usecase.course.CreateCourseUseCase;
+import coursesystem.application.usecase.course.FindByIdCourseUseCase;
 import coursesystem.domain.repository.CourseRepository;
 import coursesystem.domain.repository.UserRepository;
 
@@ -18,5 +19,10 @@ public class CourseUseCaseConfig {
   @Bean 
   CreateCourseUseCase createCourseUseCase(CourseApplicationMapper mapper, UserRepository userRepository, CourseRepository repository) {
     return new CreateCourseUseCase(mapper, userRepository, repository);
+  }
+
+  @Bean 
+  FindByIdCourseUseCase findByIdCourseUseCase(CourseApplicationMapper mapper, CourseRepository repository) {
+    return new FindByIdCourseUseCase(mapper, repository);
   }
 }
