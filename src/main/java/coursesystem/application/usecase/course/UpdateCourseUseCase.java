d0a9c82e7fa5bd861ba6161806ba8c75efdaccc0
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-14 03:39:25 -0300

    feat(UpdateCourseUseCase): add validateUser function

--- src/main/java/coursesystem/application/usecase/course/UpdateCourseUseCase.java
+++ src/main/java/coursesystem/application/usecase/course/UpdateCourseUseCase.java
@@ -1,28 +1,44 @@
 package coursesystem.application.usecase.course;
 
+import java.util.Optional;
+
+import coursesystem.application.exceptions.BusinessException;
 import coursesystem.application.exceptions.NotFoundException;
 import coursesystem.application.input.course.UpdateCourseInputDTO;
 import coursesystem.application.mapper.CourseApplicationMapper;
 import coursesystem.application.output.course.CourseOutputDTO;
 import coursesystem.domain.model.course.Course;
+import coursesystem.domain.model.usercourse.UserCourse;
+import coursesystem.domain.model.usercourse.UserType;
 import coursesystem.domain.repository.CourseRepository;
+import coursesystem.domain.repository.UserCourseRepository;
 
 public class UpdateCourseUseCase {
   private final CourseApplicationMapper mapper;
+  private final UserCourseRepository userCourseRepository;
   private final CourseRepository repository;
 
   public UpdateCourseUseCase(CourseApplicationMapper mapper,
+                              UserCourseRepository userCourseRepository,
                               CourseRepository repository) {
     this.mapper = mapper;
+    this.userCourseRepository = userCourseRepository;
     this.repository = repository;
   }
 
   public CourseOutputDTO execute(UpdateCourseInputDTO input) {
     Course course = this.repository.findById(input.id()).orElseThrow(() -> new NotFoundException("Course Not Found."));
 
+    this.validateUser(input);
     course = this.mapper.update(course, input);
     this.repository.save(course);
 
     return this.mapper.toOutput(course);
+  }
+
+  private void validateUser(UpdateCourseInputDTO input) {
+    Optional<UserCourse> userCourse = this.userCourseRepository.findByUserIdAndCourseId(input.userId(), input.id());
+
+    if (userCourse.isEmpty() || userCourse.get().userType() != UserType.OWNER) throw new BusinessException("Invalid User.");
   }
 }
