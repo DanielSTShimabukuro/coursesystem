2c126810c2da0517a273e65d1248fc913c25217a
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-13 21:23:25 -0300

    feat(UserCourseRepository): add findUserCourseByUserIdAndCourseId function

--- src/main/java/coursesystem/domain/repository/UserCourseRepository.java
+++ src/main/java/coursesystem/domain/repository/UserCourseRepository.java
@@ -1,10 +1,13 @@
 package coursesystem.domain.repository;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import coursesystem.domain.model.usercourse.UserCourse;
 
 public interface UserCourseRepository {
+  Optional<UserCourse> findUserCourseByUserIdAndCourseId(UUID userId, UUID courseId);
+
   Set<UserCourse> findAllByUserId(UUID userId);
 }
