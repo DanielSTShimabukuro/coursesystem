7fb7574eb69054fa29ab81a2f651df462c2de212
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-14 03:20:01 -0300

    feat(UserCourseRepositoryImpl): add findAllByUserId function

--- src/main/java/coursesystem/application/input/course/UpdateCourseInputDTO.java
+++ src/main/java/coursesystem/application/input/course/UpdateCourseInputDTO.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 public record UpdateCourseInputDTO(UUID id,
                                     String name,
                                     String description,
-                                    BigDecimal price) {
+                                    BigDecimal price,
+                                    UUID userId) {
   
 }
