8d0351a2859e40a332647d4f4c8689ebd12cd10a
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-14 03:16:00 -0300

    feat(UserCourseJpaRepository): add findAllByUserId

--- src/main/java/coursesystem/infrastructure/persistence/repository/userCourse/UserCourseJpaRepository.java
+++ src/main/java/coursesystem/infrastructure/persistence/repository/userCourse/UserCourseJpaRepository.java
@@ -1,5 +1,6 @@
 package coursesystem.infrastructure.persistence.repository.userCourse;
 
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import coursesystem.infrastructure.persistence.entity.UserCourseJpaEntity;
 
 public interface UserCourseJpaRepository extends JpaRepository<UserCourseJpaEntity, UUID> {
-  
+  Set<UserCourseJpaEntity> findAllByUserId(UUID userId);
 }
