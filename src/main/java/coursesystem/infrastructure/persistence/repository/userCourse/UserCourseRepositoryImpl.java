4112597c3a29ab824327bde98dfea3e9d4eac74b
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-14 03:27:39 -0300

    feat(UserCourseRepositoryImpl): add findByUserIdAndCourseId function

---
+++ src/main/java/coursesystem/infrastructure/persistence/repository/userCourse/UserCourseRepositoryImpl.java
@@ -0,0 +1,27 @@
+package coursesystem.infrastructure.persistence.repository.userCourse;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import coursesystem.domain.model.usercourse.UserCourse;
+import coursesystem.domain.repository.UserCourseRepository;
+import coursesystem.infrastructure.persistence.mapper.UserCoursePersistenceMapper;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor 
+public class UserCourseRepositoryImpl implements UserCourseRepository {
+  private final UserCoursePersistenceMapper mapper;
+  private final UserCourseJpaRepository jpaRepository;
+
+  @Override 
+  public Optional<UserCourse> findByUserIdAndCourseId(UUID userId, UUID courseId) {
+    return this.jpaRepository.findByUserIdAndCourseId(userId, courseId).map(userCourse -> this.mapper.toDomain(userCourse));
+  }
+
+  @Override 
+  public Set<UserCourse> findAllByUserId(UUID userId) {
+    return this.jpaRepository.findAllByUserId(userId).stream().map(userCourse -> this.mapper.toDomain(userCourse)).collect(Collectors.toSet());
+  }
+}
