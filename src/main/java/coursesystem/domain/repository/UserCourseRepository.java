1b9ec9e38e5e321d4f32d529711681b545adaa2e
DanielSTShimabukuro <daniel.s.t.shimabukuro@gmail.com>
2026-09-20 06:47:43 -0300

    feat(UserCourseRepository): add save function

--- src/main/java/coursesystem/domain/repository/UserCourseRepository.java
+++ src/main/java/coursesystem/domain/repository/UserCourseRepository.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 import coursesystem.domain.model.usercourse.UserCourse;
 
 public interface UserCourseRepository {
+  UserCourse save(UserCourse userCourse);
+
   boolean existsByUserIdAndCourseId(UUID userId, UUID courseId);
 
   Optional<UserCourse> findByUserIdAndCourseId(UUID userId, UUID courseId);
